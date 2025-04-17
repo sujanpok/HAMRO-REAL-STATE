e0336589369146fb3fdd9a0732afafcdea2764f7
sujan pokharel <97655524+sujanpok@users.noreply.github.com>
2025-04-18 00:23:37 +0900

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: "postgresql://neondb_owner:npg_GrX0A7HTNPDd@ep-snowy-star-a8xe92jt-pooler.eastus2.azure.neon.tech/neondb?sslmode=require",
   ssl: { rejectUnauthorized: false }, // Required for Neon
 });
 
