e92a094587508dc9c26db249a91f29152b860d58
Homerblack <93103378+Homerblack@users.noreply.github.com>
2025-04-28 18:05:46 +0900

    Develop (#15)
    * logIN page and nav bar change
    
    * two Files Deleted
    
    * logInpageChange
    
    * Bohara work (#13)
    
    * two Files Deleted
    
    * logInpageChange
    
    ---------
    
    Co-authored-by: Homerblack <subarnabohara@gmail.com.com>
    
    * littleChanges of log in and sign up
    
    ---------
    
    Co-authored-by: Homerblack <subarnabohara@gmail.com.com>

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import logo from "../images/logo.jpg";
 import styled from 'styled-components';
-import { FiShoppingCart} from 'react-icons/fi';
+import { FiShoppingCart } from 'react-icons/fi';
 import { CgMenu } from "react-icons/cg";
 //import { useAddCartContext } from './context/AddCartContext';
 
@@ -16,7 +16,7 @@ const Navbar = () => {
     setActiveMobile(false);
   };
 
-  const navbarToggle = () =>{
+  const navbarToggle = () => {
     if (activeMobile === false) {
       setActiveMobile(true);
     } else {
@@ -29,40 +29,43 @@ const Navbar = () => {
     <Wrapper >
       <div className="containter-fluid main_menu">
         <div>
-                <nav className="navbar navbar-expand-lg">
-                <Link className="navbar-brand" to="/">
-                <img src = {logo}  alt='logo'/></Link>
-                    <div className='mobile-navbar-btn'>
-                        <CgMenu name= 'menu-outline' className='mobile-nav-icon menu-outline' onClick={() => navbarToggle()} />
-                    </div>  
-                    <div className= {`navbar-collapse ${activeMobile === false ? 'collapse' : ''}`} id="navbarSupportedContent">
-                      <ul className="navbar-nav ml-auto">
-                       <li className={`nav-link ${activeLink === '/' ? 'active' : ''}`}>
-                         <Link className="nav-link" to="/" onClick={() => handleLinkClick('/')}>Home</Link>
-                       </li>
-                       <li className={`nav-link ${activeLink === '/about' ? 'active' : ''}`}>
-                         <Link className="nav-link" to="/about" onClick={() => handleLinkClick('/about')}>About</Link>
-                       </li>
-                       <li className={`nav-link ${activeLink === '/contact' ? 'active' : ''}`}>
-                         <Link className="nav-link" to="/contact" onClick={() => handleLinkClick('/contact')}>Contact</Link>
-                       </li>
-                       <li className={`nav-link ${activeLink === '/product' ? 'active' : ''}`}>
-                         <Link className="nav-link" to="/product" onClick={() => handleLinkClick('/product')}>Product</Link>
-                       </li>
-                       <li className={`nav-link ${activeLink === '/signup' ? 'active' : ''}`}>
-                         <Link className="nav-link" to="/logout" onClick={() => handleLinkClick('/logout')}>Logout</Link>
-                       </li>
-                       <li>
-                         <Link className="nav-link cart-trolley-link" to="/cart" ><FiShoppingCart className='cart-trolley'/>
-                        {/* <span className='cart-total-item'>{total_cart_item}</span>*/}
-                        </Link>
-                       </li>
-                      </ul>  
-                                 
-                    </div>
-                  </nav>
+          <nav className="navbar navbar-expand-lg">
+            <Link className="navbar-brand" to="/">
+              <img src={logo} alt='logo' /></Link>
+            <div className='mobile-navbar-btn'>
+              <CgMenu name='menu-outline' className='mobile-nav-icon menu-outline' onClick={() => navbarToggle()} />
             </div>
+            <div className={`navbar-collapse ${activeMobile === false ? 'collapse' : ''}`} id="navbarSupportedContent">
+              <ul className="navbar-nav ml-auto">
+                <li className={`nav-link ${activeLink === '/' ? 'active' : ''}`}>
+                  <Link className="nav-link" to="/" onClick={() => handleLinkClick('/')}>Home</Link>
+                </li>
+                <li className={`nav-link ${activeLink === '/about' ? 'active' : ''}`}>
+                  <Link className="nav-link" to="/about" onClick={() => handleLinkClick('/about')}>About</Link>
+                </li>
+                <li className={`nav-link ${activeLink === '/contact' ? 'active' : ''}`}>
+                  <Link className="nav-link" to="/contact" onClick={() => handleLinkClick('/contact')}>Contact</Link>
+                </li>
+                <li className={`nav-link ${activeLink === '/product' ? 'active' : ''}`}>
+                  <Link className="nav-link" to="/product" onClick={() => handleLinkClick('/product')}>Product</Link>
+                </li>
+                <li className={`nav-link ${activeLink === '/login' ? 'active' : ''}`}>
+                  <Link className="nav-link" to="/login" onClick={() => handleLinkClick('/login')}>Login</Link>
+                </li>
+                <li className={`nav-link ${activeLink === '/logout' ? 'active' : ''}`}>
+                  <Link className="nav-link" to="/logout" onClick={() => handleLinkClick('/logout')}>Logout</Link>
+                </li>
+                <li>
+                  <Link className="nav-link cart-trolley-link" to="/cart" >
+                    <FiShoppingCart className='cart-trolley' />
+                  </Link>
+                </li>
+              </ul>
+
+            </div>
+          </nav>
         </div>
+      </div>
     </Wrapper>
   )
 }
@@ -104,7 +107,7 @@ img {
   display: none;
 }
 {/*
-@media (max-width: ${({theme}) => theme.media.mobile}) {
+@media (max-width: ${({ theme }) => theme.media.mobile}) {
   .mobile-navbar-btn {
     display: inline-block;
     z-index: 9999;
